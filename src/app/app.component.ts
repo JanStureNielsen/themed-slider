84234bcd56adb45914701d0f016643d8d17483fe
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-03-17 16:49:59 -0600

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [MatSliderModule, CommonModule],
+
 })
 export class AppComponent {
-  title = 'slider-example';
+  useOtherTheme: boolean = true;
+  useOtherColor: boolean = true;
+
+  public palette() {
+      const palette = this.useOtherColor ? "primary" : "accent";
+
+      console.log('jsn: palette:', palette);
+
+      return palette;
+  }
+
+  public theme() {
+      return this.useOtherTheme ? "use-light-theme" : "";
+  }
+
 }
