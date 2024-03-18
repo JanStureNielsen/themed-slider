17fc1d985df5b07c417b9a412e75a4305c01310f
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-03-17 22:58:16 -0600

    Add selector for color palette within theme.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
@@ -8,15 +11,17 @@ import { MatSliderModule } from '@angular/material/slider';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [MatSliderModule, CommonModule],
-
+  imports: [ CommonModule, FormsModule, MatRadioModule, MatSliderModule, NgFor ],
 })
 export class AppComponent {
+  paletteNames: string[] = ['primary', 'accent', 'warn'];
+  paletteSelected: string = 'primary';
+
   useOtherTheme: boolean = true;
   useOtherColor: boolean = true;
 
   public palette() {
-      const palette = this.useOtherColor ? "primary" : "accent";
+      const palette = this.paletteSelected;
 
       console.log('jsn: palette: ', palette);
 
@@ -24,7 +29,7 @@ export class AppComponent {
   }
 
   public theme() {
-      const theme = this.useOtherTheme ? "use-light-theme" : "";
+      const theme = this.useOtherTheme ? 'use-light-theme' : '';
 
       console.log('jsn: theme: ', theme);
 
