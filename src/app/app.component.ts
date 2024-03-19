abb6874d8a35c752f6519f021cd0fb413a4e1e88
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-03-19 13:58:22 -0600

    Use ThemePalette instead of strings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { NgFor } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSliderModule } from '@angular/material/slider';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
@@ -26,7 +27,7 @@ export class AppComponent {
   themeNames: string[] = ['theme-1', 'theme-2', 'theme-3'];
   themeSelected: string = this.themeNames[0];
 
-  paletteNames: string[] = ['primary', 'accent', 'warn'];
-  paletteSelected: string = this.paletteNames[0];
+  paletteNames: ThemePalette[] = ['primary', 'accent', 'warn'];
+  paletteSelected: ThemePalette = this.paletteNames[0];
 
 }
