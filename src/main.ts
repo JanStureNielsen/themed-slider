61d37c85d8fdb973a690ff9f9c0dfb38e9c5c1c2
Jan Nielsen <jan.sture.nielsen@gmail.com>
2024-08-01 10:53:20 -0600

    Upgrade to Angular 18

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {importProvidersFrom} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
-import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {VERSION as CDK_VERSION} from '@angular/cdk';
 import {VERSION as MAT_VERSION, MatNativeDateModule} from '@angular/material/core';
